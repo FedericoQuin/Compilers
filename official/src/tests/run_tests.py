458106d87af8169a9a30a7cd5dde07f93080a069
SamMylle <mylle.sam@gmail.com>
2017-04-06 17:14:29 +0200

    Added test framework (pytest) to travis + dummy test

--- official/src/tests/run_tests.py
+++ official/src/tests/run_tests.py
@@ -1,3 +1,10 @@
 from antlr4 import *
 
-print("Hello World!")
+#print("Hello World!")
+
+def func(x):
+    return x + 1
+
+def test_answer():
+	# Example test, see https://docs.pytest.org/en/latest/getting-started.html#getstarted for more
+    assert func(3) == 4
