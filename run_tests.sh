919b37dae6da9d4a45fe4b6f54727e290c8f1969
SamMylle <mylle.sam@gmail.com>
2017-04-07 13:26:21 +0200

    Fixed tests again

--- run_tests.sh
+++ run_tests.sh
@@ -3,6 +3,14 @@ sh compileLanguage.sh cGrammar
 if [ "$?" -eq 0 ]; then
 	cd ..
 	pytest ./official/src/tests/run_tests.py
+
+	if [ "$?" -eq 0 ]; then
+		echo "OK"
+	else
+		echo "NOK"
+	fi
+
+	exit "$?"
 fi
 
 exit 1
