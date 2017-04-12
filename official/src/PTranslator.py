f14ca8ce26c5a9faa534562a99e29b9560adcbeb
FedericoQuin <federico-quin@hotmail.com>
2017-04-13 00:29:38 +0200

    Added support for entering and leaving scopes when constructing ST from AST

--- official/src/PTranslator.py
+++ official/src/PTranslator.py
@@ -16,14 +16,41 @@ class PTranslator:
     def fillSymbolTable(self, filename):
         astwalker = ASTWalker(self.AST)
         nodes = astwalker.getNodesDepthFirst()
-        currentLevel = 0
 
-        for node in nodes:
+        currentLevel = 0
+        # Here comes the fun part :D
+        levelList = []
+        
+
+        for entry in nodes:
+            node = entry[0]
+            nodeLevel = entry[1]
+
+            # If the node we are visiting now is on the same/higher level than the current working scope -> leave the scope/multiple scopes
+            # (with the exception of the global scope)
+            if (len(levelList) != 0) and (nodeLevel <= levelList[-1]):
+                currentDepth = len(levelList)
+
+                # Slice all the levels deeper and equal to the current level of the node
+                # For example:  levelList = [0 1 5 6 8]
+                #               nodeLevel = 5       (eg. an int declaration after a code block on level 5 in the AST)
+                #
+                # Result:       levelList = levelList[:2] = [0 1]   (the code block from level 5, as well as the local scopes beyond, should be dropped)
+                levelList = levelList[:levelList.index(nodeLevel)]
+
+                # Leave the amount of scopes that are not used anymore
+                for i in range(0, currentDepth - len(levelList)):
+                    self.symbolTable.leaveScope()
+                # Update the currentLevel value to the updated scopes
+                currentLevel = len(levelList)
+
             if (node.type == ASTNodeType.Block):
                 currentLevel += 1
+                levelList.append(nodeLevel)
                 self.symbolTable.enterScope()
-            if (node.type == ASTNodeType.Function):
+            elif (node.type == ASTNodeType.Function):
                 currentLevel += 1
+                levelList.append(nodeLevel)
                 self.symbolTable.enterScope()
             elif (node.type == ASTNodeType.FloatDecl):
                 self.symbolTable.insertEntry(str(node.value), "float", Scope.GLOBAL if currentLevel == 0 else Scope.LOCAL)
--- official/src/py/AST/ASTWalker.py
+++ official/src/py/AST/ASTWalker.py
@@ -8,16 +8,22 @@ class ASTWalker:
 		self.AST = ast
 
 	def traverseDepthFirst(self):
+		# The queue stores tuples of the node, and on which level in the AST it belongs
 		queue = []
-		queue.append(self.AST.root)
+		queue.append((self.AST.root, 0))
 
 		while (len(queue) != 0):
 			currentNode = queue.pop(0)
-			for node in reversed(currentNode.children):
-				queue.insert(0,node)
+			for node in reversed(currentNode[0].children):
+				queue.insert(0,(node, currentNode[1] + 1))
 			self.nodes.append(currentNode)
 
 	def getNodesDepthFirst(self):
+		"""
+			Returns a list with tuples of the form (NODE, LEVEL), where
+				NODE = The node in the AST
+				LEVEL = The level in the AST where the node was found
+		"""
 		self.traverseDepthFirst()
 		return self.nodes
 			
