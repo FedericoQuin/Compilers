9fdc028bc720c01b287436d6bdd0241464953abf
FedericoQuin <federico-quin@hotmail.com>
2017-04-12 22:36:24 +0200

    Added support for global decls in p translator

--- official/src/PTranslator.py
+++ official/src/PTranslator.py
@@ -16,18 +16,21 @@ class PTranslator:
     def fillSymbolTable(self, filename):
         astwalker = ASTWalker(self.AST)
         nodes = astwalker.getNodesDepthFirst()
+        currentLevel = 0
 
         for node in nodes:
             if (node.type == ASTNodeType.Block):
+                currentLevel += 1
                 self.symbolTable.enterScope()
             if (node.type == ASTNodeType.Function):
+                currentLevel += 1
                 self.symbolTable.enterScope()
             elif (node.type == ASTNodeType.FloatDecl):
-                self.symbolTable.insertEntry(str(node.value), "float")
+                self.symbolTable.insertEntry(str(node.value), "float", Scope.GLOBAL if currentLevel == 0 else Scope.LOCAL)
             elif (node.type == ASTNodeType.IntDecl):
-                self.symbolTable.insertEntry(str(node.value), "int")
+                self.symbolTable.insertEntry(str(node.value), "int", Scope.GLOBAL if currentLevel == 0 else Scope.LOCAL)
             elif (node.type == ASTNodeType.CharDecl):
-                self.symbolTable.insertEntry(str(node.value), "char")
+                self.symbolTable.insertEntry(str(node.value), "char", Scope.GLOBAL if currentLevel == 0 else Scope.LOCAL)
             
 
         # save the symbol table to a text file (might be temporary)
