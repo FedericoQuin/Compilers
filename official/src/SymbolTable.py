7e88326e9bf1a37ff7729e41b92565da174928de
FedericoQuin <federico-quin@hotmail.com>
2017-04-12 22:31:15 +0200

    Added address allocation for variables in symbol table (not sure if needed there)

--- official/src/SymbolTable.py
+++ official/src/SymbolTable.py
@@ -8,6 +8,11 @@ class SymbolTable:
 		Class used to store the symbol table when constructing the program text.
 		Used implementation: hash tables for different scopes.
 	"""
+
+	# Static variable to hold current Allocation Address for new symbols, denoted in bytes.
+	AllocationAddress = 0
+	
+	
 	def __init__(self):
 		self.globalScopeTable = {}
 		self.localScopeTables = []
@@ -20,11 +25,17 @@ class SymbolTable:
 		"""
 			Insert an entry into the symbol table, at the specified scope (GLOBAL or LOCAL).
 		"""
+
+		# assign enough memory, depending on the type of the symbol
+		# NOTE global variables should not collide with local variables,
+		#	   since global variables can only be inserted when no local scope is open
+		beginAddress = self.assignAddress(_type)
+
 		if (scope == Scope.LOCAL):
 			assert len(self.localScopeTables) != 0
-			self.localScopeTables[-1].addSymbol(symbol, _type)
+			self.localScopeTables[-1].addSymbol(symbol, _type, beginAddress)
 		else:
-			self.globalScopeTable[symbol] = _type
+			self.globalScopeTable[symbol] = SymbolMapping(_type, beginAddress)
 		
 	def lookupSymbol(self, symbol):
 		"""
@@ -38,12 +49,14 @@ class SymbolTable:
 		"""
 			Enters a new scope.
 		"""
-		self.localScopeTables.append(STSingleScope())
+		self.localScopeTables.append(STSingleScope(SymbolTable.AllocationAddress))
 
 	def leaveScope(self):
 		"""
 			Leaves the current local scope.
 		"""
+		# 'free' up all the memory addresses used in the local scope first
+		SymbolTable.AllocationAddress = self.localScopeTables[-1].getBeginAddress()
 		del self.localScopeTables[-1]
 
 	def searchSymbolLocal(self, symbol):
@@ -58,15 +71,29 @@ class SymbolTable:
 
 		return None
 
-		
-		
+	def assignAddress(self, _type):
+		"""
+			Provides (virtual) space for a variable of type _type.
+		"""
+		# TODO add support for pointers
+		address = SymbolTable.AllocationAddress
+
+		if _type == "int":
+			SymbolTable.AllocationAddress += 4 
+		elif _type == "float":
+			SymbolTable.AllocationAddress += 4 
+		elif _type == "char":
+			SymbolTable.AllocationAddress += 1
+			
+		return address
 
 class STSingleScope:
 	"""
 		Class used to store the symbol table for a single scope segment.
 	"""
-	def __init__(self):
+	def __init__(self, beginAddress):
 		self.table = {}
+		self.beginAddress = beginAddress
 
 	def __str__(self):
 		return str(self.table)
@@ -77,6 +104,23 @@ class STSingleScope:
 	def lookupSymbol(self, symbol):
 		return self.table[symbol]
 
-	def addSymbol(self, symbol, _type):
-		self.table[symbol] = _type
+	def addSymbol(self, symbol, _type, addr = 0):
+		self.table[symbol] = SymbolMapping(_type, addr)
 	
+	def getSymbolCount(self):
+		return len(self.table)
+
+	def getBeginAddress(self):
+		return self.beginAddress
+
+
+class SymbolMapping:
+	def __init__(self, _type, addr):
+		self.type = _type
+		self.addr = addr
+	
+	def __str__(self):
+		return "(" + str(self.type) + ", " + str(self.addr) + ")"
+
+	def __repr__(self):
+		return str(self)
