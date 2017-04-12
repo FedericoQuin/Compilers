59b984274bc0afbf3d9fe98c83ed3b2e1db2a6b6
FedericoQuin <federico-quin@hotmail.com>
2017-04-13 01:14:38 +0200

    Few bugfixes, ST is now saved every time scope(s) get deleted

--- official/src/PTranslator.py
+++ official/src/PTranslator.py
@@ -16,27 +16,36 @@ class PTranslator:
     def fillSymbolTable(self, filename):
         astwalker = ASTWalker(self.AST)
         nodes = astwalker.getNodesDepthFirst()
-
         currentLevel = 0
-        # Here comes the fun part :D
         levelList = []
-        
 
-        for entry in nodes:
-            node = entry[0]
-            nodeLevel = entry[1]
+        if (filename != ""):
+            self.saveSymbolTable(filename, 'w')
 
+
+        for (node, nodeLevel) in nodes:
             # If the node we are visiting now is on the same/higher level than the current working scope -> leave the scope/multiple scopes
             # (with the exception of the global scope)
             if (len(levelList) != 0) and (nodeLevel <= levelList[-1]):
+                if (filename != ""):
+                    self.saveSymbolTable(filename, 'a')
                 currentDepth = len(levelList)
+
+                indexToSlice = 0
+                if not(nodeLevel in levelList):
+                    # Find the nearest match in the levelList
+                    for level in reversed(levelList):
+                        if (level < nodeLevel):
+                            indexToSlice = levelList.index(level) + 1
+                else:
+                    indexToSlice = levelList.index(nodeLevel)
 
                 # Slice all the levels deeper and equal to the current level of the node
                 # For example:  levelList = [0 1 5 6 8]
                 #               nodeLevel = 5       (eg. an int declaration after a code block on level 5 in the AST)
                 #
                 # Result:       levelList = levelList[:2] = [0 1]   (the code block from level 5, as well as the local scopes beyond, should be dropped)
-                levelList = levelList[:levelList.index(nodeLevel)]
+                levelList = levelList[:indexToSlice]
 
                 # Leave the amount of scopes that are not used anymore
                 for i in range(0, currentDepth - len(levelList)):
@@ -44,29 +53,42 @@ class PTranslator:
                 # Update the currentLevel value to the updated scopes
                 currentLevel = len(levelList)
 
+            def enterScope():
+                # little helper function, quite repetitive otherwise
+                nonlocal currentLevel
+                nonlocal levelList
+
+                currentLevel += 1
+                levelList.append(nodeLevel)
+                self.symbolTable.enterScope()
+
             if (node.type == ASTNodeType.Block):
-                currentLevel += 1
-                levelList.append(nodeLevel)
-                self.symbolTable.enterScope()
+                enterScope()
             elif (node.type == ASTNodeType.Function):
-                currentLevel += 1
-                levelList.append(nodeLevel)
-                self.symbolTable.enterScope()
+                enterScope()
+            elif (node.type == ASTNodeType.IfTrue):
+                enterScope()
+            elif (node.type == ASTNodeType.IfFalse):
+                enterScope()
+            elif (node.type == ASTNodeType.While):
+                enterScope()
+            elif (node.type == ASTNodeType.For):
+                enterScope()
+                
             elif (node.type == ASTNodeType.FloatDecl):
                 self.symbolTable.insertEntry(str(node.value), "float", Scope.GLOBAL if currentLevel == 0 else Scope.LOCAL)
             elif (node.type == ASTNodeType.IntDecl):
                 self.symbolTable.insertEntry(str(node.value), "int", Scope.GLOBAL if currentLevel == 0 else Scope.LOCAL)
             elif (node.type == ASTNodeType.CharDecl):
                 self.symbolTable.insertEntry(str(node.value), "char", Scope.GLOBAL if currentLevel == 0 else Scope.LOCAL)
-            
 
         # save the symbol table to a text file (might be temporary)
         if (filename != ""):
-            self.saveSymbolTable(filename)
+            self.saveSymbolTable(filename, 'a')
 
-    def saveSymbolTable(self, filename):
-        symbolTableFile = open(filename, 'w')
-        symbolTableFile.write(str(self.symbolTable))
+    def saveSymbolTable(self, filename, mode):
+        symbolTableFile = open(filename, mode)
+        symbolTableFile.write(str(self.symbolTable) + "\n\n")
         symbolTableFile.close()
 
     def saveProgram(self, filename):
