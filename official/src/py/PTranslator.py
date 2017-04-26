b2801a490586705bc8a1bf3ab614202bb11ba639
FedericoQuin <federico-quin@hotmail.com>
2017-04-26 14:01:10 +0200

    Cosmetic.

--- official/src/py/PTranslator.py
+++ official/src/py/PTranslator.py
@@ -9,35 +9,31 @@ class PTranslator:
     def __init__(self):
         self.AST = None
         self.programText = ""
-        self.symbolTableBuilder = None
-        self.symbolTable = None
-        self.typeChecker = None
-        self.existenceChecker = None
 
     def translate(self, ast, symbolTableFileName="", printDescription=False):
         self.AST = ast
-        self.symbolTable = SymbolTable()
-        self.symbolTableBuilder = SymbolTableBuilder(self.symbolTable)
-        self.existenceChecker = ExistenceChecker(self.symbolTable)
+        symbolTable = SymbolTable()
+        symbolTableBuilder = SymbolTableBuilder(symbolTable)
+        existenceChecker = ExistenceChecker(symbolTable)
 
         astwalker = ASTWalker(self.AST)
         nodes = astwalker.getNodesDepthFirst()
 
         # Existence checking
         for (node, nodeLevel) in nodes:
-            self.symbolTableBuilder.processNode(node, nodeLevel)
-            self.existenceChecker.checkExistence(node)
+            symbolTableBuilder.processNode(node, nodeLevel)
+            existenceChecker.checkExistence(node)
 
-        self.symbolTable = SymbolTable()
-        self.symbolTableBuilder = SymbolTableBuilder(self.symbolTable, symbolTableFileName, printDescription)
-        self.typeChecker = TypeChecker(self.symbolTable)
+        symbolTable = SymbolTable()
+        symbolTableBuilder = SymbolTableBuilder(symbolTable, symbolTableFileName, printDescription)
+        typeChecker = TypeChecker(symbolTable)
 
         # Type checking and actual translation
         for (node, nodeLevel) in nodes:
-            self.symbolTableBuilder.processNode(node, nodeLevel)
-            self.typeChecker.checkType(node)
+            symbolTableBuilder.processNode(node, nodeLevel)
+            typeChecker.checkType(node)
 
-        self.symbolTableBuilder.saveSymbolTable("a")
+        symbolTableBuilder.saveSymbolTable("a")
 
 
 
